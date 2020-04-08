f34d7c579d06575a031d456d0ba63267f2ac3d0a
GusNajera <gvt_a9@hotmail.com>
2020-04-08 15:14:55 -0500

    post request ready

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 const URL = 'mongodb+srv://onceonceonce11:onceonceonce11@nemo-rkcm5.mongodb.net/inventory?retryWrites=true&w=majority';
-mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const schema = new mongoose.Schema({
     item: { type: String, required: true },
@@ -37,6 +36,22 @@ app.get('/', (req, res) => {
 
     });
 });
+
+app.post('/', (req, res) => {
+    mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true});
+    const InventoryItem = mongoose.model('Inventory', schema, 'items');
+    const db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'connection error'));
+    db.once('open', () => {
+        let Dummy = new InventoryItem( {...req.body} );
+        
+        Dummy.save()
+            .then( () => res.send({ uploaded: true, ...req.body}) )
+            .catch( (err) => res.send({ uploaded: false, ...err }) )
+    });
+});
+
 
 app.listen(3000, ()=>{
     console.log('Server on port 3000');
